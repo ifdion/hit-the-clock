360a0180fc9261805f15862bec69182afc0c8a9b
Dion <priyandiono@99.co>
2022-11-14 23:00:09 +0000

    feat(hit) update logic

--- hit/index.js
+++ hit/index.js
@@ -11,12 +11,23 @@ yargs(hideBin(process.argv))
     .parse();
 const { argv } = yargs;
 
+const mainLog = [];
+function log(message) {
+    const now = new Date();
+    mainLog.push(`${message} : ${now.toUTCString()} `);
+    console.log({ status: message});
+}
 
 (async () => {
+    const now = new Date();
+    
+    const day = now.getDay();
     const mode = argv.out ? 'clockOut' : 'clockIn';
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const context = browser.defaultBrowserContext();
+
+    log('setting up browser');
 
     await context.overridePermissions(process.env.MACHINE_PATH, ['geolocation']);
 
@@ -27,6 +38,8 @@ const { argv } = yargs;
     });
 
     try {
+        log('logging in to talenta');
+
         await page.goto(process.env.AUTH_PATH);
         await page.type('#user_email', process.env.EMAIL);
         await page.type('#user_password', process.env.PASSWORD);
@@ -35,27 +48,51 @@ const { argv } = yargs;
         await page.goto(process.env.MACHINE_PATH, {
             waitUntil: 'networkidle0'
         });
-        await page.setGeolocation({ latitude: parseFloat(process.env.LATITUDE), longitude: parseFloat(process.env.LONGITUDE) });
-        await Promise.all([
-            page.waitForNavigation({ waitUntil: 'networkidle0' }),
-            page.click('[href="/live-attendance"]'),
 
-        ])
+        log('set up geolocation');
+
+        const coordinate = (day > 1 && day < 5 ? process.env.COORDINATE_WFO : process.env.COORDINATE_WFH).split(',');
+
+        await page.setGeolocation({ latitude: parseFloat(coordinate[0]), longitude: parseFloat(coordinate[1]) });
+
+        log('go to live attendance');
+
+        await page.click('[href="/live-attendance"]'),
+        await page.waitForSelector('.d-flex.justify-content-between.mb-3')
+
+        log('click clock in / clock out button');
+
         if (mode === 'clockIn') {
             await page.mouse.click(400, 540)
         } else {
             await page.mouse.click(640, 540)
         }
+
+        log('logging out of talenta');
+
         await page.screenshot({ path: 'talenta.png' });
         await page.goto(process.env.LOGOUT_PATH);
         await browser.close();
 
+        log('send notification');
+
         axios.post(process.env.SLACK_WEBHOOK_URL, {
-            text: `Congratulation, <${process.env.SLACK_HANDLE}> ! You just ${mode === 'clockIn' ? 'clocked in' : 'clocked out'}. Lets do some serious code! Check manually here ${process.env.MACHINE_PATH}.`
+            text: `Congratulation, <${process.env.SLACK_HANDLE}> ! You just ${mode === 'clockIn' ? 'clocked in' : 'clocked out'}. Lets do some serious code! Check manually here ${process.env.MACHINE_PATH}.`,
+            attachments: [
+                {
+                    color: '#557D23',
+                    text: mainLog.join("\n")
+                }
+            ]
         })
 
-        console.log({ status: 'success' });
+        console.log({ status: 'success', mainLog });
     } catch (error) {
+        log('error catch')
+        console.error({ status: 'error', mainLog });
+
+        await page.screenshot({ path: 'talenta.png' });
+
         await page.goto(process.env.LOGOUT_PATH);
         await browser.close();
 
@@ -64,11 +101,13 @@ const { argv } = yargs;
             attachments: [
                 {
                     color: '#93254F',
-                    text: error.message || JSON.stringify(error)
+                    text: error.message?  error.message : JSON.stringify(error)
+                },
+                {
+                    color: '#93254F',
+                    text: JSON.stringify(log)
                 }
             ]
         })
-
-        console.debug(error);
     }
 })();
